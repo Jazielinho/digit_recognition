c4de37054210144826de58b02d64f7ba93b9c6fd
jazielinho <jahazielponce@gmail.com>
2021-04-18 00:34:51 +0200

    digit

--- digit_app.py
+++ digit_app.py
@@ -1,102 +1,110 @@
-
+'''
+author: jazielinho
+'''
 
 import pandas as pd
 import numpy as np
 import cv2
 import tensorflow as tf
-from lime import lime_image
-from lime.wrappers.scikit_image import SegmentationAlgorithm
 import streamlit as st
 from streamlit_drawable_canvas import st_canvas
 from sklearn.model_selection import train_test_split
-from sklearn.neighbors import NearestNeighbors
 from skimage.segmentation import mark_boundaries
-from skimage.color import gray2rgb, rgb2gray, label2rgb
+from skimage.color import label2rgb
 import matplotlib.pyplot as plt
-
-from typing import Tuple
+import seaborn as sns
+import pickle
+import dill
 
 import config
+
 
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
 
+
+
+# =================================================================================
+
 model = None
 emb_model = None
 X_train = None
+y_train = None
 nearest_model = None
 explainer = None
 segmenter = None
+umap_model = None
+umap_train_df = None
 
+
+# =================================================================================
+
+# PREPARANDO IMAGEN
 
 def to_rgb(x):
+    ''' Convertimos una imagen de escala de grises a RGB'''
     x_rgb = np.zeros((x.shape[0], 28, 28, 3))
     for i in range(3):
         x_rgb[..., i] = x[..., 0]
     return x_rgb.reshape(-1, 28, 28, 3)
 
 
-def load_model() -> Tuple[tf.keras.Model, tf.keras.Model]:
-    ''' Cargando el modelo '''
+def prepara_img(image_array):
+    ''' preparamos una imagen para predecir el dígito '''
+    img = cv2.resize(image_array, (28, 28))
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = img.reshape(-1, 28, 28, 1)
+    return to_rgb(img)
+
+
+# =================================================================================
+# CARGANDO Y PREDICIENDO LOS DIGITOS
+
+def load_model():
+    ''' Cargando el modelo RED NEURONAL '''
     global model
     global emb_model
-    if model is None:
+    if model is None or emb_model is None:
         model_file = open(config.MODEL_PATH_JSON, 'r')
         model = model_file.read()
         model_file.close()
         model = tf.keras.models.model_from_json(model)
         model.load_weights(config.MODEL_PATH_H5)
 
-        emb_model = tf.keras.models.Model(model.input, model.get_layer('embedding').output)
-
+        emb_model = tf.keras.models.Model(model.input,
+                                          model.get_layer('embedding').output)
     return model, emb_model
 
 
-def load_nearest_model():
-    global nearest_model
-    global X_train
-    global model
-    global emb_model
-
-    if nearest_model is None:
-        model, emb_model = load_model()
-
-        (X, y), (_, _) = tf.keras.datasets.mnist.load_data()
-        X = X.reshape(-1, 28, 28, 1)
-
-        X_train, _, y_train, _ = train_test_split(X, y, test_size=0.2, random_state=123456)
-        X_train = to_rgb(X_train)
-
-        emb_train = emb_model.predict(X_train)
-
-        nearest_model = NearestNeighbors(n_neighbors=config.N_NEIGHBORS, metric='cosine')
-        nearest_model.fit(emb_train)
-
-        return X_train, nearest_model
-
-
-def load_explainer():
-    global explainer
-    global segmenter
-    if explainer is None:
-        explainer = lime_image.LimeImageExplainer(verbose=False, random_state=12345)
-        segmenter = SegmentationAlgorithm('quickshift', kernel_size=1, max_dist=200, ratio=0.2)
-
-        return explainer, segmenter
-
-
-# ================================================================================================================
-
 def predict_class(img):
+    ''' Calculamos y graficamos las predicciones '''
     global model
     global emb_model
     model, emb_model = load_model()
     predictions = model.predict(img)
-    pred_df = pd.Series(predictions.ravel(), index=range(10))
-    return pred_df
+    predictions = predictions.ravel()
+    clase_predicha = int(predictions.argmax())
+    prob_ = 100 * predictions[clase_predicha]
+    pred_df = pd.DataFrame(predictions, index=range(10))
+    st.subheader(f"Clase predicha: {clase_predicha}, probabilidad: {prob_:.2f}")
+    st.bar_chart(pred_df)
 
 
-def get_rules(img):
+# =================================================================================
+# DECISIONES USANDO LIME
+
+def load_explainer():
+    ''' Cargando el modelo LIME '''
+    global explainer
+    global segmenter
+    if explainer is None or segmenter is None:
+        explainer = dill.load(open(config.MODEL_EXPLAINER, 'rb'))
+        segmenter = dill.load(open(config.MODEL_SEGMENTER, 'rb'))
+    return explainer, segmenter
+
+
+def plot_rules(img):
+    ''' Obtenemos las reglas de decisión usando LIME '''
     global model
     global emb_model
     global explainer
@@ -106,64 +114,146 @@ def get_rules(img):
 
     X_eval = img.reshape(28, 28, 3)
 
-    explanation = explainer.explain_instance(X_eval, classifier_fn=model.predict, top_labels=10, hide_color=0,
-                                             num_samples=100, segmentation_fn=segmenter)
+    explanation = explainer.explain_instance(X_eval,
+                                             classifier_fn=model.predict,
+                                             top_labels=10,
+                                             hide_color=0,
+                                             num_samples=100,
+                                             segmentation_fn=segmenter)
 
     plt.figure(figsize=(15, 10))
 
     for i in range(10):
-        temp, mask = explanation.get_image_and_mask(i, positive_only=True, num_features=1000, hide_rest=False,
-                                                    min_weight=0.01)
+        temp, mask = explanation.get_image_and_mask(i,
+                                                   positive_only=True,
+                                                   num_features=1000,
+                                                   hide_rest=False,
+                                                   min_weight=0.01)
         plt.subplot(2, 5, (i + 1))
-        plt.imshow(label2rgb(mask.astype(np.uint8), X_eval.astype(np.uint8), bg_label=0), interpolation='nearest')
-        plt.title('Positive for {}'.format(i))
-        plt.axis('off')
-
-    plt.axis('off')
+        plt.imshow(label2rgb(mask.astype(np.uint8),
+                             X_eval.astype(np.uint8),
+                             bg_label=0),
+                   interpolation="nearest")
+        plt.title(f"Positivo para clase: {i}")
+        plt.axis("off")
+    plt.axis("off")
 
     st.pyplot()
 
-    image, mask = explanation.get_image_and_mask(model.predict(X_eval.reshape((1, 28, 28, 3))).argmax(axis=1)[0],
-                                                 positive_only=True, hide_rest=False)
-
+    clase_predicha = model.predict(X_eval.reshape((1, 28, 28, 3))).argmax(axis=1)[0]
+    image, mask = explanation.get_image_and_mask(clase_predicha,
+                                                 positive_only=True,
+                                                 hide_rest=False)
     plt.imshow(X_eval.astype(np.uint8))
     plt.imshow(mark_boundaries(image.astype(np.uint8), mask))
+    plt.title(f"Decisiones para la clase predicha: {clase_predicha}")
+    plt.axis("off")
     st.pyplot()
 
 
-def get_similars(img):
+# =================================================================================
+# REDUCCIÓN DE DIMENSIONES USANDO UMAP
+
+def load_umap():
+    '''Cargando el modelo UMAP '''
+    global umap_model
+    global umap_train_df
+    if umap_model is None or umap_train_df is None:
+        umap_model = pickle.load(open(config.MODEL_UMAP, 'rb'))
+        umap_train_df = pd.read_csv(config.UMAP_TRAIN)
+    return umap_model, umap_train_df
+
+
+def plot_umap(img):
+    ''' Reducción de dimensiones usando UMAP '''
     global model
     global emb_model
-    global nearest_model
-    global X_train
+    global umap_model
+    global umap_train_df
     model, emb_model = load_model()
+    umap_model, umap_train_df = load_umap()
+    emb_test = emb_model.predict(img.reshape(-1, 28, 28, 3), verbose=0)
+    emb_test = emb_test.reshape(1, -1)
+    umap_test = umap_model.transform(emb_test)
+    umap_test = umap_test.reshape(1, -1)
+
+    plt.figure(figsize=(12, 10))
+    sns.scatterplot(x="x0", y="x1", alpha=0.1, hue="target",
+                    legend="full", data=umap_train_df,
+                    palette="Paired_r")
+    plt.scatter(umap_test[0, 0], umap_test[0, 1], s=100, c="k")
+    st.pyplot()
+
+
+# =================================================================================
+# IMAGENES SIMILARES USANDO NearestNeighbors
+
+def load_data():
+    ''' Cargando los datos '''
+    global X_train
+    global y_train
+    if X_train is None or y_train is None:
+        (X, y), (X_test, y_test) = tf.keras.datasets.mnist.load_data()
+        X = X.reshape(-1, 28, 28, 1)
+        X_train, X_val, y_train, y_test = train_test_split(X, y,
+                                                           test_size=0.2,
+                                                           random_state=123456)
+        X_train = to_rgb(X_train)
+        X_train = X_train.reshape(-1, 28, 28, 3)
+    return X_train, y_train
+
+
+def load_nearest_model():
+    ''' Cargando modelo de vecinos cercanos '''
+    global nearest_model
+    if nearest_model is None:
+        nearest_model = pickle.load(open(config.MODEL_SKLEARN_NN, 'rb'))
+    return nearest_model
+
+
+def plot_similares(img):
+    ''' Imágenes similares usando NearestNeighbors '''
+    global model
+    global emb_model
+    global X_train
+    global y_train
+    global nearest_model
+    model, emb_model = load_model()
+    X_train, y_train = load_data()
+    nearest_model = load_nearest_model()
+
     emb_test = emb_model.predict(img.reshape(1, 28, 28, 3), verbose=0)
     emb_test = emb_test.reshape(1, -1)
 
-    X_train, nearest_model = load_nearest_model()
     distances_test, pred_nearest_test = nearest_model.kneighbors(emb_test)
 
     pred_nearest_test = pred_nearest_test.ravel()
 
     plt.figure(figsize=(15, 10))
 
-    for i in range(len(pred_nearest_test)):
-        plt.subplot(1, config.N_NEIGHBORS, (i + 1))
-        plt.imshow(X_train[pred_nearest_test[i]].astype(np.uint8), cmap=plt.cm.binary)
-
-    plt.axis('off')
-
+    for i in range(nearest_model.n_neighbors):
+        plt.subplot(1, nearest_model.n_neighbors, (i + 1))
+        plt.imshow(X_train[pred_nearest_test[i]].astype(np.uint8),
+                   cmap=plt.cm.binary)
+        plt.title(y_train[pred_nearest_test[i]])
+        plt.axis("off")
+    plt.axis("off")
     st.pyplot()
 
 
+# =================================================================================
 
+st.title("¡RECONOCE DÍGITOS!")
 
+st.markdown("""
+La siguiente aplicación intenta predecir el dígito escrito.
+* Usamos redes neuronales convolucionales con Tensorflow.
+* Para identificar reglas de decisión usamos LIME.
+* Para reducir dimensiones usamos UMAP.
+* Para buscar imágenes similares usamos NearestNeighbors.
+""")
 
-# ================================================================================================================
-st.title('My Digit Recognizer')
-st.markdown(''' Try to write a digit! ''')
-
-mode = st.checkbox("Draw (or Delete)?", True)
+st.markdown("""¡ESCRIBA UN DÍGITO, INTENTARÉ PREDECIRLO!""")
 
 canvas_result = st_canvas(
     fill_color='#000000',
@@ -172,23 +262,25 @@ canvas_result = st_canvas(
     background_color='#000000',
     width=config.SIZE_DRAW,
     height=config.SIZE_DRAW,
-    drawing_mode="freedraw" if mode else "transform",
-    key='canvas')
+    drawing_mode='freedraw',
+    key='canvas'
+)
 
 
 if canvas_result.image_data is not None:
-    if st.button('Predict'):
-        img = cv2.resize(canvas_result.image_data.astype(np.uint8), (28, 28))
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        img = img.reshape(-1, 28, 28, 1)
-        img = to_rgb(img)
+    if st.button("PREDECIR"):
+        image_array = canvas_result.image_data.astype(np.uint8)
+        img = prepara_img(image_array=image_array)
 
-        st.text("Predicción")
-        pred_df = predict_class(img)
-        st.bar_chart(pred_df)
+        st.subheader("PREDICCIÓN")
+        predict_class(img)
 
-        st.text("Reglas")
-        get_rules(img)
+        st.subheader("LIME PARA REGLAS DE DECISIÓN")
+        plot_rules(img)
 
-        st.text("Imágenes similares")
-        get_similars(img)
+        st.subheader("UMAP PARA REDUCCIÓN DE DIMENSIONES")
+        plot_umap(img)
+
+        st.subheader("VECINOS CERCANOS PARA BUSCAR IMÁGENES SIMILARES EN EL CONJUNTO DE ENTRENAMIENTO")
+        plot_similares(img)
+
